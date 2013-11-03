f2416e511367bd80f3bf3e434794e14baf38c0e3
rektide <rektide@voodoowarez.com>
2013-11-03 17:24:42 -0500

    Create Vertx instance if needed in JVertx. Remove container paramter.

--- src/main/java/com/voodoowarez/commodyne/init/dynjs/JVertx.java
+++ src/main/java/com/voodoowarez/commodyne/init/dynjs/JVertx.java
@@ -4,18 +4,20 @@ import org.dynjs.Config;
 import org.dynjs.runtime.DynJS;
 import org.dynjs.runtime.GlobalObject;
 import org.vertx.java.core.Vertx;
-import org.vertx.java.platform.Container;
+import org.vertx.java.core.VertxFactory;
 
 import com.voodoowarez.commodyne.RuntimeInitializer;
 
 public class JVertx implements RuntimeInitializer {
 
-	protected Container container;
 	protected Vertx vertx;
 
-	public JVertx(Vertx vertx, Container container){
+	public JVertx(Vertx vertx){
 		this.vertx= vertx;
-		this.container= container;
+	}
+
+	public JVertx(){
+		this(VertxFactory.newVertx());
 	}
 
 	public void prepConfig(Config config) {
@@ -24,6 +26,5 @@ public class JVertx implements RuntimeInitializer {
 	public void initialize(DynJS dynjs) {
 		final GlobalObject globalObject = dynjs.getExecutionContext().getGlobalObject();
 		globalObject.defineReadOnlyGlobalProperty("__jvertex", this.vertx);
-		globalObject.defineReadOnlyGlobalProperty("__jcontainer", this.container);
 	}
 }
