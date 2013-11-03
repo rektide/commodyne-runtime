da048fd44fc6413010efbc936538e9bc98e83543
rektide <rektide@voodoowarez.com>
2013-11-03 17:45:25 -0500

    Run DynJS initializers in Nodyn initializers.

--- src/main/java/com/voodoowarez/commodyne/init/Nodyn.java
+++ src/main/java/com/voodoowarez/commodyne/init/Nodyn.java
@@ -7,6 +7,12 @@ import com.voodoowarez.commodyne.init.nodyn.Npm;
 
 public class Nodyn extends MultiRuntimeInitializerRunner {
 
+	static {
+		final RuntimeInitializer[] inits = new RuntimeInitializer[DynJS.INITIALIZERS.length+Nodyn.INITIALIZERS.length];
+		System.arraycopy(DynJS.INITIALIZERS, 0, inits, 0, DynJS.INITIALIZERS.length);
+		System.arraycopy(Nodyn.INITIALIZERS, 0, inits, 0, Nodyn.INITIALIZERS.length);
+		INITIALIZERS = inits;
+	}
 	private static RuntimeInitializer[] INITIALIZERS = new RuntimeInitializer[] { new Global(), new Npm(), new DynNodeJs() };
 
 	public Nodyn(RuntimeInitializer[] initializers) {
